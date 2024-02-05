184200deae76e110bc2415724797e22333232114
Tiago Lima <tiago.lima@oli.live>
2024-02-05 18:32:32 -0300

    feat: add AsyncStorage to get storage from app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { OliSdk } from 'oli-react-native-sdk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [clicks, setClicks] = useState(0);
+
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('my-key', value);
+      await AsyncStorage.setItem('oli-key', value);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  useEffect(() => {
+    storeData('test-oli-sdk-huehue');
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>#FicaMarcelinha</Text>
